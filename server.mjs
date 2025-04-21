8fb28ab7104d4303a26f9165057b33debce6bd60
Jmsteele01 <Jmsteele01@hotmail.com>
2025-04-20 20:32:21 -0600

    connecting users and posts to server

--- server.mjs
+++ server.mjs
@@ -1,16 +1,17 @@
 //Imports
 import express from 'express';
 const router = express.Router();
-// import users from "../data/users.mjs";
-// import posts from "../data/posts.mjs";
+import users from "../data/users.mjs";
+import posts from "../data/posts.mjs";
 
 //Setups
 const app = express();
 const port = 3000;
+
 //Middleware
 // Importing the data from our fake database files.
-// const users = require("./data/users");
-// const posts = require("./data/posts");
+const users = require("./data/users");
+const posts = require("./data/posts");
 
 //Routes
 app.get("/", (req, res) => {
@@ -20,4 +21,5 @@ app.get("/", (req, res) => {
 //Listener
 app.listen(port, () => {
   console.log(`server running on port: ${port}`);
+  console.log(users);
 });
