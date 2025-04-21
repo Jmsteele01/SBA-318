29c46363d1fcd840984584714c2d7a11c9ffa822
Jmsteele01 <Jmsteele01@hotmail.com>
2025-04-20 20:58:21 -0600

    basic server setup complete

--- routes/userRoutes.mjs
+++ routes/userRoutes.mjs
@@ -6,3 +6,14 @@
 // })
 
 // export default router;
+import express from "express";
+import users from "../data/users.mjs";
+import posts from "../data/posts.mjs";
+
+const router = express.Router();
+
+router.get('/', (req, res) =>{
+    res.send("User Routes")
+})
+
+export default router;
--- server.mjs
+++ server.mjs
@@ -4,25 +4,28 @@ const router = express.Router();
 import users from "./data/users.mjs";
 import posts from "./data/posts.mjs";
 
-//testing
 
 //Setups
 const app = express();
 const port = 3000;
 
 //Middleware
-// Importing the data from our fake database files.
-// const users = require("./data/users");
-// const posts = require("./data/posts");
 
 //Routes
-// app.get("/", (req, res) => {
-//   res.send("User Routes");
-// });   
+app.get("/", (req, res) => {
+  res.send("User Routes");
+});
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.get("/posts", (req, res) => {
+  res.json(posts);
+});
 
 //Listener
 app.listen(port, () => {
   console.log(`server running on port: ${port}`);
-  console.log(users);
-  console.log(posts);
+  console.log("Users: ", users);
+  console.log("Posts: ", posts);
 });
