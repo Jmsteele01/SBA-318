c7c3836e61b44a25ce4d74cea78f247fd927e5ce
Jmsteele01 <Jmsteele01@hotmail.com>
2025-04-20 20:42:25 -0600

    posts and users successfully outputted.

--- server.mjs
+++ server.mjs
@@ -1,8 +1,10 @@
 //Imports
 import express from 'express';
 const router = express.Router();
-import users from "../data/users.mjs";
-import posts from "../data/posts.mjs";
+import users from "./data/users.mjs";
+import posts from "./data/posts.mjs";
+
+//testing
 
 //Setups
 const app = express();
@@ -10,16 +12,17 @@ const port = 3000;
 
 //Middleware
 // Importing the data from our fake database files.
-const users = require("./data/users");
-const posts = require("./data/posts");
+// const users = require("./data/users");
+// const posts = require("./data/posts");
 
 //Routes
-app.get("/", (req, res) => {
-  res.send("User Routes");
-});   
+// app.get("/", (req, res) => {
+//   res.send("User Routes");
+// });   
 
 //Listener
 app.listen(port, () => {
   console.log(`server running on port: ${port}`);
   console.log(users);
+  console.log(posts);
 });
