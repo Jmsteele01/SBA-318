5212dd8e4e7490b43a54bba605eee3ea9b138a1f
Jmsteele01 <Jmsteele01@hotmail.com>
2025-04-20 20:22:41 -0600

    basic user data

--- server.mjs
+++ server.mjs
@@ -1,20 +1,23 @@
+//Imports
 import express from 'express';
 const router = express.Router();
 // import users from "../data/users.mjs";
 // import posts from "../data/posts.mjs";
 
-//const express = require("express");
+//Setups
 const app = express();
 const port = 3000;
-
+//Middleware
 // Importing the data from our fake database files.
 // const users = require("./data/users");
 // const posts = require("./data/posts");
 
+//Routes
 app.get("/", (req, res) => {
   res.send("User Routes");
 });   
 
+//Listener
 app.listen(port, () => {
   console.log(`server running on port: ${port}`);
 });
